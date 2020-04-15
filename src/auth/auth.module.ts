d835e79019753f355c2c22aef1a4660eec1a8d3f
Sebastian Bejga <sebastian.bejga@iteratec.com>
2020-04-16 00:57:09 +0200

    jwt_secret without configService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,19 +4,15 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TicketingModule } from '../ticketing/ticketing.module';
-import { ConfigService } from '@nestjs/config';
 
 @Module({
     imports: [
         PassportModule,
         // loading JwtModule async to have access to process.env via ConfigService
         JwtModule.registerAsync({
-            useFactory: async (configService: ConfigService) => ({
-                secret:
-                    configService.get<string>('JWT_SECRET') ||
-                    'ShVmYq3t6w9y$B&E)H@McQfTjWnZr4u7',
+            useFactory: async () => ({
+                secret: process.env.JWT_SECRET || 'ShVmYq3t6w9y$B&E)H@McQfTjWnZr4u7',
             }),
-            inject: [ConfigService],
         }),
         // TODO: move HashingService
         TicketingModule,
