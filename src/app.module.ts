eecc12dd50c1be9f55614dbcc67e3613bad19b65
Sebastian Bejga <sebastian.bejga@iteratec.com>
2020-04-15 23:40:41 +0200

    auth module create jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,24 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TicketingModule } from './ticketing/ticketing.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     TicketingModule,
+    AuthModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    const MONGODB_URI = process.env.MONGODB_URI;
+    if (!MONGODB_URI) {
+      console.error("no MONGODB_URI found. Please set.");
+      process.exit(1);
+    }
+  }
+}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,39 @@
+import { Module, Logger } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { TicketingModule } from '../ticketing/ticketing.module';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+    imports: [
+        PassportModule,
+        // loading JwtModule async to have access to process.env via ConfigService
+        JwtModule.registerAsync({
+            useFactory: async (configService: ConfigService) => ({
+              secret: configService.get<string>('JWT_SECRET') || "ShVmYq3t6w9y$B&E)H@McQfTjWnZr4u7",
+            }),
+            inject: [ConfigService],
+          }),
+        // TODO: move HashingService
+        TicketingModule,
+    ],
+    providers: [
+        AuthService,
+    ],
+    exports: [
+        AuthService
+    ],
+    controllers: [AuthController]
+  })
+  export class AuthModule {
+
+    private readonly logger = new Logger(AuthModule.name);
+
+    constructor() {
+        if (!process.env.JWT_SECRET) {
+            this.logger.warn("no JWT_SECRET found.")
+        }
+    }
+  }
