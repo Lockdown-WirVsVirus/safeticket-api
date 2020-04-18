416f8cee3f623135ccc2d10b12980b2d1ca50e07
Philipp Müller-Leven <p.muellerleven@gmail.com>
2020-04-18 20:47:54 +0200

    Add import form ticketing to crypto module

--- src/ticketing/ticketing.module.ts
+++ src/ticketing/ticketing.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TicketsController } from './controller/tickets.controller';
-import { ticketSchema } from './services/tickets.schema';
+import { ticketSchema, TICKET_MODEL_NAME } from './services/tickets.schema';
 import { TicketsService } from './services/tickets.service';
+import { CryptoModule } from '../crypto/crypto.module';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'Tickets', schema: ticketSchema }])],
+    imports: [MongooseModule.forFeature([{ name: TICKET_MODEL_NAME, schema: ticketSchema }]), CryptoModule],
     controllers: [TicketsController],
     providers: [TicketsService],
     exports: [TicketsService],
