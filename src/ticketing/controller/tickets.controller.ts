80a87d46618df8213b2d729352b81e5ec8baf78f
Philipp Müller-Leven <p.muellerleven@gmail.com>
2020-04-21 12:42:45 +0200

    Add Result type for propper error handling in service.

--- src/ticketing/controller/tickets.controller.ts
+++ src/ticketing/controller/tickets.controller.ts
@@ -1,23 +1,51 @@
-import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, Param, Post, Logger } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { HashingService } from '../../crypto/services/hashing.service';
-import { TicketsService } from '../services/tickets.service';
+import { TicketsService, TicketCreationFailureReason } from '../services/tickets.service';
 import { IdentityDto, TicketRequestDto, TicketResponseDto } from './tickets.dto';
 
 @ApiTags('ticket')
 @Controller('api/v1/tickets')
 export class TicketsController {
+    private readonly logger = new Logger(TicketsController.name);
+
     constructor(private readonly ticketsService: TicketsService, private readonly hashingService: HashingService) {}
 
     @Post()
     async createTicket(@Body() ticketDto: TicketRequestDto): Promise<TicketResponseDto> {
-        return this.ticketsService.createTicket({
+        const createdTicketResult = await this.ticketsService.createTicket({
             hashedPassportId: await this.hashingService.hashPassportId(ticketDto.passportId),
             reason: ticketDto.reason,
             startAddress: ticketDto.startAddress,
             endAddress: ticketDto.endAddress,
             validFromDateTime: ticketDto.validFromDateTime,
             validToDateTime: ticketDto.validToDateTime,
+        });
+
+        return new Promise((resolve, reject) => {
+            createdTicketResult
+                .mapErr(ticketCreationFailure => {
+                    switch (ticketCreationFailure.reason) {
+                        case TicketCreationFailureReason.ConflictInTime:
+                            return reject(new HttpException('Ticket conflicts in same time for other ticket', HttpStatus.CONFLICT));
+                        default:
+                            this.logger.error('Failure during creation:' + ticketCreationFailure?.error);
+                            return reject(new HttpException('Failure during creation of ticket', HttpStatus.INTERNAL_SERVER_ERROR));
+                    }
+                })
+                .map(successfullyCreatedTicket => {
+                    this.logger.log(`Created new ticket: ${successfullyCreatedTicket.ticketId}`);
+                    resolve({
+                        ticketId: successfullyCreatedTicket.ticketId,
+                        ticketStatus: successfullyCreatedTicket.ticketStatus,
+                        validFromDateTime: successfullyCreatedTicket.validFromDateTime,
+                        validToDateTime: successfullyCreatedTicket.validToDateTime,
+                        hashedPassportId: successfullyCreatedTicket.hashedPassportId,
+                        reason: successfullyCreatedTicket.reason,
+                        startAddress: successfullyCreatedTicket.startAddress,
+                        endAddress: successfullyCreatedTicket.endAddress,
+                    });
+                });
         });
     }
 
--- src/ticketing/controller/tickets.dto.ts
+++ src/ticketing/controller/tickets.dto.ts
@@ -1,21 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Length } from 'class-validator';
+import { Length, IsNotEmpty, MinLength, IsDateString, isNotEmpty, IsDataURI, isDateString } from 'class-validator';
 import { Address, Identity, Ticket, TicketStatus } from '../services/tickets.service';
 
 export class AddressDto implements Address {
     @ApiProperty()
+    @IsNotEmpty()
     street: string;
 
     @ApiProperty()
+    @IsNotEmpty()
     houseNumber: string;
 
     @ApiProperty()
+    @Length(5)
+    @IsNotEmpty()
     zipCode: string;
 
     @ApiProperty()
+    @IsNotEmpty()
     city: string;
 
     @ApiProperty()
+    @MinLength(2)
     country: string;
 }
 
@@ -23,25 +29,31 @@ export class TicketRequestDto {
     @ApiProperty({
         description: 'The mandatory id of passport for verification',
     })
+    @IsNotEmpty()
     passportId: string;
 
     @ApiProperty({ description: 'The reason why the ticket has been requested.' })
     reason: string;
 
     @ApiProperty({ type: AddressDto, description: 'The address where you want to start with your ticket.' })
+    @IsNotEmpty()
     startAddress: AddressDto;
 
     @ApiProperty({ type: AddressDto, description: 'The address where you want to end with your ticket.' })
+    @IsNotEmpty()
     endAddress: AddressDto;
 
     @ApiProperty({ description: 'The date where you want to the ticket to become active.' })
+    @IsDateString()
     validFromDateTime: Date;
 
     @ApiProperty({ description: 'The date where you want to activate the ticket.' })
+    @IsDateString()
     validToDateTime: Date;
 }
 
 export class IdentityDto implements Identity {
+    @IsNotEmpty()
     @Length(24, 24)
     @ApiProperty({ description: 'The hashed passport id which has been generated during creation of ticket' })
     hashedPassportId: string;
@@ -56,18 +68,26 @@ export class TicketResponseDto implements Ticket {
     hashedPassportId: string;
 
     @ApiProperty()
+    @IsNotEmpty()
     reason: string;
 
     @ApiProperty({ type: AddressDto })
+    @IsNotEmpty()
     startAddress: AddressDto;
+
     @ApiProperty({ type: AddressDto })
+    @IsNotEmpty()
     endAddress: AddressDto;
 
     @ApiProperty()
+    @IsDateString()
     validFromDateTime: Date;
+
     @ApiProperty()
+    @IsDateString()
     validToDateTime: Date;
 
     @ApiProperty({ readOnly: true })
+    @IsNotEmpty()
     ticketStatus: TicketStatus;
 }
